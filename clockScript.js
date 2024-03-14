1dd80fd403a247b5590cd72b8936a956dcb6910c
CSmith3s <CSmith3s@capetigers.com>
2024-03-14 10:27:37 -0500

    dumb

--- clockScript.js
+++ clockScript.js
@@ -8,33 +8,33 @@ const toggle = document.querySelector('.toggle')
 const days = ["Sunday", "Monday", "Tuesday", "Wenesday", "Thrusday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
-toggle.addEventListener('click', (e) => {
+toggle.addEventListener("click", (e) => {
     const html = document.querySelector('html')
     if(html.classList.contains('dark')) {
-        html.classList.remove('dark')
-        e.target.innerHTML = 'Dark mode'
+        html.classList.remove('dark');
+        e.target.innerHTML = 'Dark mode';
     } else {
-        html.classList.add('dark')
-        e.target.innerHTML = 'Light mode'
+        html.classList.add('dark');
+        e.target.innerHTML = 'Light mode';
     }
-})
+});
 
 function setTime() {
     const time = new Date()
-    const month = time.getMonth{}
-    const day = time.getDay{}
-    const date = time.getDate{}
+    const month = time.getMonth()
+    const day = time.getDay()
+    const date = time.getDate()
     const hoursForClock = hours % 12
-    const minutes = time.getMinutes{}
-    const seconds = time.getSeconds{}
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
     const ampm = hours >= 12 ? 'PM' : 'AM'
 
     hourEl.style.tranform = 'transform: translate(-50%, -100%) rotate(${scale(hoursForClock,0,11,0,360)}deg);'
     minuteEl.style.tranform = 'transform: translate(-50%, -100%) rotate(${scale(hoursForClock,0,59,0,360)}deg);'
     secondEl.style.tranform = 'transform: translate(-50%, -100%) rotate(${scale(hoursForClock,0,11,0,360)}deg);'
 
-    timeEl.innerHTML = '${hoursForClock}:${minutes < 10 ? '0${minutes}' : minutes} ${ampm}'
-    dateEl.innerHTML = '${days[day]} <span class=circle>{$date}</span>'
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]} <span class=circle>{$date}</span>`
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
